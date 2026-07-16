2464008a2b02e14cd98bb2ba72023cc9c65bfc7d
Omar El Saadi <Omaralielsaadi@gmail.com>
2026-07-16 16:42:46 +0100

    Add data access layer for Countries and People
    Added `DVLD_DataAccess.csproj` with project configurations and references. Defined assembly metadata in `AssemblyInfo.cs`. Implemented `clsCountryData` for database operations on the `Countries` table, including retrieval and existence checks. Implemented `clsPerson` for database operations on the `People` table, including CRUD operations and existence checks.

---
+++ DVLD_DataAccess/clsCountryData.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsCountryData
+    {
+        public static bool GetCountryInfoByID(int ID, ref string CountryName)
+        {
+
+            string Query = @"SELECT [CountryID],[CountryName] FROM [dbo].[Countries] WHERE CountryID = @CountryID";
+
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand(Query, Connection)) 
+            { 
+
+                command.Parameters.AddWithValue("@CountryID", ID);
+                try
+                {
+                    Connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+
+                            CountryName = (string)reader["CountryName"];
+                            return true;
+                        }
+                        else
+                        {
+                            // The record was not found
+                            return  false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+
+        public static bool GetCountryInfoByName(string CountryName, ref int ID)
+        {
+
+            string Query = @"SELECT [CountryID],[CountryName] FROM [dbo].[Countries] WHERE CountryName = @CountryName";
+
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand(Query, Connection))
+            {
+
+                command.Parameters.AddWithValue("@CountryName", CountryName);
+                try
+                {
+                    Connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+
+                            ID = (int)reader["CountryID"];
+                            return true;
+                        }
+                        else
+                        {
+                            // The record was not found
+                            return false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+
+
+        public static DataTable GetAllCountries()
+        {
+
+            DataTable dt = new DataTable();
+
+            string query = "SELECT * FROM Countries order by CountryName";
+            using (SqlConnection connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        dt.Load(reader);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+            return dt;
+        }
+    
+        public static bool IsCountryExist(int ID)
+        {
+
+            string query = "SELECT Found=1 FROM Countries WHERE CountryID = @CountryID";
+
+            using (SqlConnection connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection)) 
+            {
+
+                command.Parameters.AddWithValue("@CountryID", ID);
+                try
+                {
+                    connection.Open();
+                    object Result = command.ExecuteScalar();
+                    return Result != null;
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error: " + ex.Message);
+                    return false;
+                }
+            }
+
+        }
+
+
+        public static bool IsCountryExist(string CountryName)
+        {
+            
+            string query = "SELECT Found=1 FROM Countries WHERE CountryName = @CountryName";
+
+            using (SqlConnection connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+
+                command.Parameters.AddWithValue("@CountryName", CountryName);
+                try
+                {
+                    connection.Open();
+                    object Result = command.ExecuteScalar();
+                    return Result != null;
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error: " + ex.Message);
+                    return false;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ DVLD_DataAccess/clsPerson.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsPersonData
+    {
+        public static bool GetPersonInfoByID(int ID,ref string NationalNo,
+            ref string FirstName,ref string SecondName ,ref string ThirdName,ref string LastName,
+            ref DateTime DateOfBirth,ref string Gender,ref string Address, ref string Email,
+            ref string Phone,ref int Nationality, ref string ImagePath)
+        {
+            string Query = @"SELECT [PersonID]
+                            ,[NationalNo]
+                            ,[FirstName]
+                            ,[SecondName]
+                            ,[ThirdName]
+                            ,[LastName]
+                            ,[DateOfBirth]
+	                        ,Gender =
+		                            CASE
+		                            	WHEN Gender=0 THEN 'Male'
+		                            	 WHEN Gender=1 THEN 'Female'
+		                            	ELSE 'Unknown'
+		                            END
+                            ,[Address]
+                            ,[Phone]
+                            ,[Email]
+                            ,[NationalityCountryID]
+                            ,[ImagePath]
+                            FROM [DVLD].[dbo].[People] where People.PersonID = @PersonID;";
+            // inner join Countries C on People.NationalityCountryID = C.CountryID
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            { 
+                 Command.Parameters.AddWithValue("@PersonID", ID);
+                try
+                {
+                    Connection.Open();
+                    using (SqlDataReader Reader = Command.ExecuteReader()) 
+                    {
+                        if (Reader.Read())
+                        {
+                            NationalNo = Reader["NationalNo"] != DBNull.Value ? (string)Reader["NationalNo"] : "";
+                            FirstName = Reader["FirstName"] != DBNull.Value ? (string)Reader["FirstName"] : "";
+                            SecondName = Reader["SecondName"] != DBNull.Value ? (string)Reader["SecondName"] : "";
+                            ThirdName = Reader["ThirdName"] != DBNull.Value ? (string)Reader["ThirdName"] : "";
+                            LastName = Reader["LastName"] != DBNull.Value ? (string)Reader["LastName"] : "";
+                            DateOfBirth = Reader["DateOfBirth"] != DBNull.Value ? (DateTime)Reader["DateOfBirth"] : DateTime.MinValue;
+                            Gender = Reader["Gender"] != DBNull.Value ? (string)Reader["Gender"] : "";
+                            Address = Reader["Address"] != DBNull.Value ? (string)Reader["Address"] : "";
+                            Phone = Reader["Phone"] != DBNull.Value ? (string)Reader["Phone"] : "";
+                            Email = Reader["Email"] != DBNull.Value ? (string)Reader["Email"] : "";
+                            Nationality = Reader["NationalityCountryID"] != DBNull.Value ? (int)Reader["NationalityCountryID"] : -1;
+                            ImagePath = Reader["ImagePath"] != DBNull.Value ? (string)Reader["ImagePath"] : "";
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        } 
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+           
+        }
+        public static bool GetPersonInfoByNationalNo(string NationalNo,ref int ID,
+           ref string FirstName, ref string SecondName, ref string ThirdName, ref string LastName,
+           ref DateTime DateOfBirth, ref string Gender, ref string Address, ref string Email,
+           ref string Phone, ref int Nationality, ref string ImagePath)
+        {
+            string Query = @"SELECT [PersonID]
+                            ,[NationalNo]
+                            ,[FirstName]
+                            ,[SecondName]
+                            ,[ThirdName]
+                            ,[LastName]
+                            ,[DateOfBirth]
+	                        ,Gender =
+		                            CASE
+		                            	WHEN Gender=0 THEN 'Male'
+		                            	 WHEN Gender=1 THEN 'Female'
+		                            	ELSE 'Unknown'
+		                            END
+                            ,[Address]
+                            ,[Phone]
+                            ,[Email]
+                            ,[NationalityCountryID]
+                            ,[ImagePath]
+                            FROM [DVLD].[dbo].[People] where People.NationalNo = @NationalNo;";// inner join Countries C on People.NationalityCountryID = C.CountryID
+
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@NationalNo", NationalNo);
+                try
+                {
+                    Connection.Open();
+                    using (SqlDataReader Reader = Command.ExecuteReader())
+                    {
+
+                    
+                        if (Reader.Read())
+                        {
+                            ID = Reader["PersonID"] != DBNull.Value ? (int)Reader["PersonID"] : -1;
+                            FirstName = Reader["FirstName"] != DBNull.Value ? (string)Reader["FirstName"] : "";
+                            SecondName = Reader["SecondName"] != DBNull.Value ? (string)Reader["SecondName"] : "";
+                            ThirdName = Reader["ThirdName"] != DBNull.Value ? (string)Reader["ThirdName"] : "";
+                            LastName = Reader["LastName"] != DBNull.Value ? (string)Reader["LastName"] : "";
+                            DateOfBirth = Reader["DateOfBirth"] != DBNull.Value ? (DateTime)Reader["DateOfBirth"] : DateTime.MinValue;
+                            Gender = Reader["Gender"] != DBNull.Value ? (string)Reader["Gender"] : "";
+                            Address = Reader["Address"] != DBNull.Value ? (string)Reader["Address"] : "";
+                            Phone = Reader["Phone"] != DBNull.Value ? (string)Reader["Phone"] : "";
+                            Email = Reader["Email"] != DBNull.Value ? (string)Reader["Email"] : "";
+                            Nationality = Reader["NationalityCountryID"] != DBNull.Value ? (int)Reader["NationalityCountryID"] : -1;
+                            ImagePath = Reader["ImagePath"] != DBNull.Value ? (string)Reader["ImagePath"] : "";
+
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                catch
+                {
+                    return false;
+                } 
+            }
+        }
+   
+
+        public static int AddNewPerson( string NationalNo,
+             string FirstName,  string SecondName,  string ThirdName,  string LastName,
+             DateTime DateOfBirth,  string Gender,  string Address,  string Email,
+             string Phone, int NationalityCountryID,  string ImagePath)
+        {
+
+            string Query = @"
+            INSERT INTO [dbo].[People]
+                        ([NationalNo]
+                        ,[FirstName]
+                        ,[SecondName]
+                        ,[ThirdName]
+                        ,[LastName]
+                        ,[DateOfBirth]
+                        ,[Gender]
+                        ,[Address]
+                        ,[Phone]
+                        ,[Email]
+                        ,[NationalityCountryID]
+                        ,[ImagePath])
+            VALUES
+                        (@NationalNo,
+                         @FirstName, 
+                         @SecondName,
+                         @ThirdName, 
+                         @LastName, 
+                         @DateOfBirth,
+                         @Gender,
+                         @Address, 
+                         @Phone, 
+                         @Email, 
+                         @NationalityCountryID,
+                         @ImagePath);
+            SELECT SCOPE_IDENTITY();";
+
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@NationalNo", NationalNo );
+                Command.Parameters.AddWithValue("@FirstName", FirstName );
+                Command.Parameters.AddWithValue("@SecondName", SecondName );
+                Command.Parameters.AddWithValue("@ThirdName", (object)ThirdName ?? DBNull.Value);
+                Command.Parameters.AddWithValue("@LastName", LastName);
+                Command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth != DateTime.MinValue ? (object)DateOfBirth : DBNull.Value);
+                Command.Parameters.AddWithValue("@Gender",Gender.Equals("male", StringComparison.OrdinalIgnoreCase) ? 0 : 1);
+                Command.Parameters.AddWithValue("@Address", Address );
+                Command.Parameters.AddWithValue("@Phone", Phone );
+                Command.Parameters.AddWithValue("@Email", (object)Email ?? DBNull.Value);
+                Command.Parameters.AddWithValue("@NationalityCountryID", NationalityCountryID);
+                Command.Parameters.AddWithValue("@ImagePath", (object)ImagePath ?? DBNull.Value);
+
+                try
+                {
+                    Connection.Open();
+                    object result = Command.ExecuteScalar();
+                    if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                    {
+                       return insertedID;
+                    }
+                }
+                catch
+                {
+                    return -1;
+                }
+                return -1;
+            }
+        }
+
+
+        public static bool UpdatePerson(int PersonID,string NationalNo,
+             string FirstName, string SecondName, string ThirdName, string LastName,
+             DateTime DateOfBirth, string Gender, string Address, string Email,
+             string Phone, int NationalityCountryID, string ImagePath)
+        {
+            string Query = @"UPDATE People
+                             SET   [NationalNo]  = @NationalNo          
+                                  ,[FirstName]   = @FirstName           
+                                  ,[SecondName]  = @SecondName          
+                                  ,[ThirdName]   = @ThirdName           
+                                  ,[LastName]    = @LastName            
+                                  ,[DateOfBirth] = @DateOfBirth         
+                                  ,[Gender] = @Gender              
+                                  ,[Address]  = @Address            
+                                  ,[Phone]    = @Phone               
+                                  ,[Email]    = @Email              
+                                  ,[NationalityCountryID] = @NationalityCountryID
+                                  ,[ImagePath] = @ImagePath
+                                    WHERE PersonID = @PersonID";
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString)) 
+            using (SqlCommand Command = new SqlCommand(Query,Connection))
+            {
+                Command.Parameters.AddWithValue("@PersonID", PersonID);
+                Command.Parameters.AddWithValue("@NationalNo", NationalNo);
+                Command.Parameters.AddWithValue("@FirstName", FirstName);
+                Command.Parameters.AddWithValue("@SecondName", SecondName);
+                Command.Parameters.AddWithValue("@ThirdName", (object)ThirdName ?? DBNull.Value);
+                Command.Parameters.AddWithValue("@LastName", LastName);
+                Command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth != DateTime.MinValue ? (object)DateOfBirth : DBNull.Value);
+                Command.Parameters.AddWithValue("@Gender", Gender.Equals("male", StringComparison.OrdinalIgnoreCase) ? 0 : 1);
+                Command.Parameters.AddWithValue("@Address", Address);
+                Command.Parameters.AddWithValue("@Phone", Phone);
+                Command.Parameters.AddWithValue("@Email", (object)Email ?? DBNull.Value);
+                Command.Parameters.AddWithValue("@NationalityCountryID", NationalityCountryID);
+                Command.Parameters.AddWithValue("@ImagePath", (object)ImagePath ?? DBNull.Value);
+                try
+                {
+                    Connection.Open();
+                    int RowAfficted = Command.ExecuteNonQuery();
+                    return RowAfficted > 0;
+                }
+                catch(Exception ex)
+                {
+                    return false;
+                }
+
+            }
+
+        }
+
+
+        public static bool DeletePersonByID(int PersonID)
+        {
+            string Query = @"DELETE FROM People
+                            WHERE PersonID = @PersonID";
+
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query,Connection))
+            {
+                Command.Parameters.AddWithValue("@PersonID", PersonID);
+                try
+                {
+                    Connection.Open();
+                    int RowAffected = Command.ExecuteNonQuery();
+                    return RowAffected > 0;
+                }
+                catch(Exception ex )
+                {
+                    return false;
+                }
+            }
+
+        }
+        public static bool DeletePersonByNationalNo(string NationalNo)
+        {
+            string Query = @"DELETE FROM People
+                            WHERE NationalNo = @NationalNo";
+
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@NationalNo", NationalNo);
+                try
+                {
+                    Connection.Open();
+                    int RowAffected = Command.ExecuteNonQuery();
+                    return RowAffected > 0;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+
+
+        }
+   
+
+        public static bool IsPersonExist(int PersonID)
+        {
+            string Query = "select Found=1 from People where PersonID = @PersonID";
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@PersonID", PersonID);
+                try
+                {
+                    Connection.Open();
+                    object Result = Command.ExecuteScalar();
+                    return Result != null;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        public static bool IsPersonExist(string NationalNo)
+        {
+            string Query = "select Found=1 from People where NationalNo = @NationalNo";
+            using (SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@NationalNo", NationalNo);
+                try
+                {
+                    Connection.Open();
+                    object Result = Command.ExecuteScalar();
+                    return Result != null;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+    
+        public static DataTable GetAllPeople()
+        {
+            DataTable dt = new DataTable();
+            string Query = @"SELECT [PersonID]
+                            ,[NationalNo]
+                            ,[FirstName]
+                            ,[SecondName]
+                            ,[ThirdName]
+                            ,[LastName]
+                            ,[DateOfBirth]
+	                        ,Gender =
+		                            CASE
+		                            	WHEN Gender=0 THEN 'Male'
+		                            	 WHEN Gender=1 THEN 'Female'
+		                            	ELSE 'Unknown'
+		                            END
+                            ,[Address]
+                            ,[Phone]
+                            ,[Email]
+                            ,[NationalityCountryID]
+                            ,[ImagePath]
+                            FROM [DVLD].[dbo].[People] order by PersonID";
+            using(SqlConnection Connection = new SqlConnection(clsDataSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query,Connection))
+            {
+                try
+                {
+                    Connection.Open();
+                    using(SqlDataReader Reader = Command.ExecuteReader())
+                    {
+                        dt.Load(Reader);
+                       
+                        return dt;
+                        
+                    }
+                }
+                catch
+                {
+                    return dt;
+                }
+            }
+        }
+    }
+}
