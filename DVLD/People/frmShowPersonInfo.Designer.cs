937de7017d3412968e6878d13f04a5b251670e2f
Omar El Saadi <Omaralielsaadi@gmail.com>
2026-07-27 19:54:00 +0100

    Add frmShowPersonInfo form to display person details

---
+++ DVLD/People/frmShowPersonInfo.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DVLD
+{
+    partial class frmShowPersonInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctrlPersonCard1 = new DVLD.People.Controls.ctrlPersonCard();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(285, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Person Details";
+            // 
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(12, 67);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(1038, 414);
+            this.ctrlPersonCard1.TabIndex = 1;
+            // 
+            // frmShowPersonInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 493);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ctrlPersonCard1);
+            this.Name = "frmShowPersonInfo";
+            this.Text = "frmPersonDetails";
+            this.Load += new System.EventHandler(this.frmPersonDetails_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private People.Controls.ctrlPersonCard ctrlPersonCard1;
+    }
+}
