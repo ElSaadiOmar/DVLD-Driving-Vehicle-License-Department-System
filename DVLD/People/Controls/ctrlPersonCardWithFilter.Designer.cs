c7c0864e854be3f07acaf80ad871d39404848ad3
Omar El Saadi <Omaralielsaadi@gmail.com>
2026-07-31 19:38:23 +0100

    Add ctrlPersonCardWithFilter to allow search by NationalNo and PersonID and Add Person

---
+++ DVLD/People/Controls/ctrlPersonCardWithFilter.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DVLD.People.Controls
+{
+    partial class ctrlPersonCardWithFilter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ctrlPersonCardWithFilter));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbSearchBy = new System.Windows.Forms.ComboBox();
+            this.txtSearchValue = new System.Windows.Forms.TextBox();
+            this.gbFilter = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnAddNewPerson = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.ctrlPersonCard1 = new DVLD.People.Controls.ctrlPersonCard();
+            this.gbFilter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find By:";
+            // 
+            // cbSearchBy
+            // 
+            this.cbSearchBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSearchBy.FormattingEnabled = true;
+            this.cbSearchBy.Items.AddRange(new object[] {
+            "National No",
+            "Person ID"});
+            this.cbSearchBy.Location = new System.Drawing.Point(159, 40);
+            this.cbSearchBy.Name = "cbSearchBy";
+            this.cbSearchBy.Size = new System.Drawing.Size(307, 34);
+            this.cbSearchBy.TabIndex = 1;
+            this.cbSearchBy.SelectedIndexChanged += new System.EventHandler(this.cbSearchBy_SelectedIndexChanged);
+            // 
+            // txtSearchValue
+            // 
+            this.txtSearchValue.Location = new System.Drawing.Point(499, 40);
+            this.txtSearchValue.Name = "txtSearchValue";
+            this.txtSearchValue.Size = new System.Drawing.Size(307, 32);
+            this.txtSearchValue.TabIndex = 2;
+            this.txtSearchValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSearchValue_KeyPress);
+            this.txtSearchValue.Validating += new System.ComponentModel.CancelEventHandler(this.txtSearchValue_Validating);
+            // 
+            // gbFilter
+            // 
+            this.gbFilter.Controls.Add(this.btnSearch);
+            this.gbFilter.Controls.Add(this.btnAddNewPerson);
+            this.gbFilter.Controls.Add(this.txtSearchValue);
+            this.gbFilter.Controls.Add(this.cbSearchBy);
+            this.gbFilter.Controls.Add(this.label1);
+            this.gbFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.gbFilter.Location = new System.Drawing.Point(22, 12);
+            this.gbFilter.Name = "gbFilter";
+            this.gbFilter.Size = new System.Drawing.Size(1006, 103);
+            this.gbFilter.TabIndex = 1;
+            this.gbFilter.TabStop = false;
+            this.gbFilter.Text = "Filter";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Transparent;
+            this.btnSearch.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSearch.BackgroundImage")));
+            this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Location = new System.Drawing.Point(836, 28);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(64, 56);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnAddNewPerson
+            // 
+            this.btnAddNewPerson.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddNewPerson.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAddNewPerson.BackgroundImage")));
+            this.btnAddNewPerson.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAddNewPerson.FlatAppearance.BorderSize = 0;
+            this.btnAddNewPerson.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddNewPerson.Location = new System.Drawing.Point(919, 28);
+            this.btnAddNewPerson.Name = "btnAddNewPerson";
+            this.btnAddNewPerson.Size = new System.Drawing.Size(64, 56);
+            this.btnAddNewPerson.TabIndex = 4;
+            this.btnAddNewPerson.UseVisualStyleBackColor = false;
+            this.btnAddNewPerson.Click += new System.EventHandler(this.btnAddNewPerson_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(4, 105);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(1040, 409);
+            this.ctrlPersonCard1.TabIndex = 0;
+            // 
+            // ctrlPersonCardWithFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbFilter);
+            this.Controls.Add(this.ctrlPersonCard1);
+            this.Name = "ctrlPersonCardWithFilter";
+            this.Size = new System.Drawing.Size(1041, 515);
+            this.gbFilter.ResumeLayout(false);
+            this.gbFilter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlPersonCard ctrlPersonCard1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbSearchBy;
+        private System.Windows.Forms.TextBox txtSearchValue;
+        private System.Windows.Forms.GroupBox gbFilter;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnAddNewPerson;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ DVLD/People/Controls/ctrlPersonCardWithFilter.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD.Helpers;
+using DVLD_Buisness;
+
+namespace DVLD.People.Controls
+{
+    public partial class ctrlPersonCardWithFilter : UserControl
+    {
+
+        public ctrlPersonCardWithFilter()
+        {
+            InitializeComponent();
+            _ApplyThem();
+            cbSearchBy.SelectedIndex = 0;
+        }
+
+
+        private void _DigitOnly_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                errorProvider1.SetError((Control)sender, "You can enter only Number!!");
+                e.Handled = true;
+            }
+            else
+            {
+                errorProvider1.SetError((Control)sender, "");
+            }
+        }
+
+
+        private bool _TextIsOnlyDigit(string Text)
+        {
+            foreach (char c in Text)
+            {
+                if (!char.IsDigit(c) && !char.IsControl(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void _DigitesOnly_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (!_TextIsOnlyDigit(txt.Text))
+            {
+                errorProvider1.SetError((Control)sender, "This Field Accept Only Number");
+                e.Cancel = true;
+            }
+
+
+        }
+
+
+        private void _ApplyThem()
+        {
+            this.BackColor = AppColors.PanelBg;
+            btnAddNewPerson.BackColor = AppColors.NavyPrimary;
+            btnSearch.BackColor = AppColors.NavyPrimary;
+        }
+        private void btnAddNewPerson_Click(object sender, EventArgs e)
+        {
+            frmAddAndUpdatePerson frm = new frmAddAndUpdatePerson();
+            frm.ShowDialog();
+        }
+
+        private void cbSearchBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtSearchValue.Text = string.Empty;
+
+        }
+
+        private void txtSearchValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (cbSearchBy.SelectedItem.ToString() == "Person ID")
+            {
+                _DigitOnly_KeyPress(sender, e);
+            }
+        }
+
+        private void txtSearchValue_Validating(object sender, CancelEventArgs e)
+        {
+            if (cbSearchBy.SelectedItem.ToString() == "Person ID")
+            {
+                _DigitesOnly_Validating(sender, e);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            switch (cbSearchBy.SelectedItem.ToString())
+            {
+                case "Person ID":
+                    {
+                        if (int.TryParse(txtSearchValue.Text, out int ID))
+                        {
+                            if (clsPerson.IsPersonExist(ID))
+                            {
+                                ctrlPersonCard1.FillTheCardWithPersonInfo(ID);
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Person With this PersonID [{ID}] was not exist","Person not found",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show($"this PersonID is Invalid", "Invalid ID", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        break;
+                    }
+                case "National No":
+                    {
+                        if (clsPerson.IsPersonExist(txtSearchValue.Text.ToString()))
+                        {
+                            ctrlPersonCard1.FillTheCardWithPersonInfo(txtSearchValue.Text.ToString());
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Person With this National No [{txtSearchValue.Text.ToString()}] was not exist", "Person not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        break;
+                    }
+            }
+        }
+    }
+}
